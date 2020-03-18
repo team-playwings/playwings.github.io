5761c3802e561868127e7b6088073ce1b3bc2dd2
JihyunKim <jihyun@squarelab.co>
2020-03-18 10:03:28 +0900

    Test

--- js/interaction.js
+++ js/interaction.js
@@ -23,16 +23,37 @@ $(function(){
 
 
 //Scroll Magic
-window.onload = function () { // 윈도우 로드를 기다리는 구문
+document.addEventListener('DOMContentLoaded', () => {
+//window.onload = function () {  윈도우 로드를 기다리는 구문
     // Init ScrollMagic
     let controller = new ScrollMagic.Controller({
         refreshInterval: 0
     });
 
+    // Intro Parallax
+    let timeline = new TimelineMax();
+    timeline
+    .to('#bg-5', 5, {y: -700})
+    .to('#bg-4', 5, {y: -500}, '-=5')
+    .to('#bg-3', 5, {y: -400}, '-=5')
+    .to('#bg-2', 5, {y: -300}, '-=5')
+    .to('#bg-1', 5, {y: -200}, '-=5')
+    .to('#section', 5, {top: '0%'}, '-=5')
+    .to('#main-contents', 5, {y: -700}, '-=5')
+
+    new ScrollMagic.Scene({
+        triggerElement: 'main',
+        duration: '200%',
+        triggerHook: 0
+    })
+    .setTween(timeline)
+    .setPin('#intro')
+    .addTo(controller);
+
     // Logo
     new ScrollMagic.Scene({
-        triggerElement: '#section-1',
-        triggerHook: 0.8
+        triggerElement: '#section',
+        triggerHook: 0.6
     })
         .setTween(TweenMax.to('#header', 0.2, {backgroundColor: '#fff'}))
         .setClassToggle('.bi-b', 'bi-bb')
@@ -41,24 +62,13 @@ window.onload = function () { // 윈도우 로드를 기다리는 구문
 
 
     // Image Fade In Up
-   
-
-    $('#sectio-1').each(function(){
-
+    $('.content-img').each(function(){
         new ScrollMagic.Scene({
             triggerElement: this,
-            triggerHook: 0.7
+            triggerHook: 1
         })
-            .setClassToggle(this, 'fade-in')
-            .addIndicators()
-            .addTo(controller);
+        .setClassToggle(this, 'fade-in')
+        .addIndicators()
+        .addTo(controller);
     });
-
-
-
-    
-
-
-
-
-};
+})
