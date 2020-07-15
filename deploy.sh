1e23f3a871b045f521383528155b1513c09fd9df
hyuntak <hyuntak@squarelab.co>
2020-07-15 20:47:08 +0900

    Clear cloudflare cache after deploy

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,11 @@ fi
 # --guess-mime-type --no-mime-magic is required. If not specified, css files that are uploaded to S3 will have "text/plain" MIME type.
 s3cmd sync --exclude-from .s3ignore _site/ $DEPLOY_URL --delete-removed --guess-mime-type --no-mime-magic
 s3cmd modify --add-header='content-type':'application/json' s3://www.playwings.co.kr/.well-known/apple-app-site-association
+
+# Clear cache in cloudfire
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" \
+  -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+  -H "X-Auth-Key: ${CLOUDFLARE_API_KEY}" \
+  -H "Content-Type: application/json" \
+  --data '{"purge_everything":true}'
+
