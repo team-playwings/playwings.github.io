42497ac191ee8622ed4230963e6f76baad669eb5
Jamie <jamie@squarelab.co>
2020-11-17 14:30:54 +0900

    Update

--- js/common.js
+++ js/common.js
@@ -30,3 +30,16 @@ function changeVideo() {
         hero[0].load();
     }
 };
+
+function getParameters() {
+    const parameters = {};
+    const vars = window.location.search.substring(1).split('&');
+    let pair;
+    for (let i = 0; i < vars.length; i++) {
+        pair = vars[i].split('=');
+        if (typeof parameters[pair[0]] === 'undefined') parameters[pair[0]] = pair[1];
+        else if (typeof parameters[pair[0]] === 'string') parameters[pair[0]] = [parameters[pair[0]], pair[1]];
+        else parameters[pair[0]].push(pair[1]);
+    }
+    return parameters;
+}
