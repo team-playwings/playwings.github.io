b9b4a4c0f4c25dc88c0a35bb2a6aa2e47142205d
jamie <jamie@squarelab.co>
2022-07-06 20:48:49 +0900

    Update

--- _site/js/deal.js
+++ _site/js/deal.js
@@ -1,42 +1,88 @@
 const menus = $('#section .lnb-wrapper .lnb a');
-const sections = gsap.utils.toArray("section");
-let currentSection = 0;
+let currentMenu = $(menus[0]);
+let type;
+let title;
+let usage_period_title;
+let usage_period_sub;
 
 menus.on('click', function (e) {
     e.preventDefault();
-    $(menus[currentSection]).removeClass('active');
-    sections.forEach((section, i) => {
-        if ($(this)[0].id.indexOf(section.id) > -1) {
-            currentSection = i;
-            $(menus[currentSection]).addClass('active');
-            section.setAttribute('style', 'display:block');
-        } else {
-            section.setAttribute('style', 'display:none');
-        }
-    });
+
+    currentMenu.removeClass('active');
+    currentMenu = $(this);
+    currentMenu.addClass('active');
+
+    type = $('#type')[0];
+    title = $('#title')[0];
+    usage_period_title = $('#usage_period_title')[0];
+    usage_period_sub = $('#usage_period_sub')[0];
+
+    switch ($(this)[0].id) {
+        case 'menu_flight':
+            type.value = '항공';
+            title.innerHTML = '항공';
+            usage_period_title.innerHTML = '탑승 기간 *';
+            usage_period_sub.innerHTML = '항공 탑승 기간을 입력해주세요.';
+            break;
+
+        case 'menu_hotel':
+            type.value = '호텔';
+            title.innerHTML = '호텔';
+            usage_period_title.innerHTML = '투숙 기간 *';
+            usage_period_sub.innerHTML = '호텔 투숙 기간을 입력해주세요.';
+            break;
+
+        case 'menu_activity':
+            type.value = '투어&액티비티';
+            title.innerHTML = '투어&액티비티';
+            usage_period_title.innerHTML = '사용 기간 *';
+            usage_period_sub.innerHTML = '상품 사용 기간을 입력해주세요.';
+            break;
+
+        case 'menu_package':
+            type.value = '패키지';
+            title.innerHTML = '패키지';
+            usage_period_title.innerHTML = '사용 기간 *';
+            usage_period_sub.innerHTML = '상품 사용 기간을 입력해주세요.';
+            break;
+    }
 });
 
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+tomorrow.setHours(0, 0, 0, 0);
+
+const theDayAfterTomorrow = new Date();
+theDayAfterTomorrow.setDate(tomorrow.getDate() + 1);
+
 $('input[name="exposure_start_date"]').daterangepicker({
+    startDate: tomorrow,
     singleDatePicker: true,
     timePicker: true,
     locale: {
         format: 'YYYY. M. D hh:mm A'
     }
 });
+
 $('input[name="selling_periods"]').daterangepicker({
-    autoApply: true,
-    locale: {
-        format: 'YYYY. M. D'
-    }
-});
-$('input[name="usage_period"]').daterangepicker({
+    startDate: tomorrow,
+    endDate: theDayAfterTomorrow,
     autoApply: true,
     locale: {
         format: 'YYYY. M. D'
     }
 });
 
-var myWidget = cloudinary.createUploadWidget({
+$('input[name="usage_period"]').daterangepicker({
+    startDate: tomorrow,
+    endDate: theDayAfterTomorrow,
+    autoApply: true,
+    locale: {
+        format: 'YYYY. M. D'
+    }
+});
+
+let myWidget = cloudinary.createUploadWidget({
         cloudName: 'kyte',
         uploadPreset: 'xm6nvbrf',
         folder: 'playwings/widget',
@@ -45,7 +91,12 @@ var myWidget = cloudinary.createUploadWidget({
         thumbnails: '.upload_multiple_images_holder'
     }, (error, result) => {
         if (!error && result && result.event === "success") {
-            console.log('Done! Here is the image info: ', result.info);
+            /*
+            if (result.info.resource_type == 'raw') {
+                let filename = result.info.public_id.substring(result.info.public_id.lastIndexOf('/'));
+                console.log(filename);
+            }
+            */
         }
     }
 )
@@ -56,7 +107,7 @@ document.getElementById("upload_widget").addEventListener("click", function () {
 
 function submitForm() {
     if (document.getElementsByClassName('upload_multiple_images_holder')[0].children.length == 0) {
-        alert("이미지를 업로드해주세요.");
+        alert("첨부파일을 업로드해주세요.");
         return;
     }
     let form = document.getElementById('form');
